05130dc6fe1f8a77c714e65298f0d787afe40b21
oshadaera68 <mindartlk010@gmail.com>
2023-05-08 22:53:06 +0530

    first commit

---
+++ src/com/devstack/pos/AppInitializer.java
@@ -0,0 +1,23 @@
+package com.devstack.pos;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LogInForm.fxml"))));
+        primaryStage.setTitle("Login");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+}
