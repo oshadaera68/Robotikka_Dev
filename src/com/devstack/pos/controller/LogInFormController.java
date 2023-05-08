e3636b782b5ca33b820565240310d76e2c0f7226
oshadaera68 <mindartlk010@gmail.com>
2023-05-08 23:04:25 +0530

    added components and finalized the ui

--- src/com/devstack/pos/controller/LogInFormController.java
+++ src/com/devstack/pos/controller/LogInFormController.java
@@ -1,4 +1,32 @@
 package com.devstack.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class LogInFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+
+    public void btnSignInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCreateAnAccOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignUpForm");
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
+    }
 }
--- src/com/devstack/pos/controller/SignUpFormController.java
+++ src/com/devstack/pos/controller/SignUpFormController.java
@@ -1,4 +1,31 @@
 package com.devstack.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SignUpFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+
+    public void btnSignUpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAlreadyOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LogInForm");
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
+    }
 }
