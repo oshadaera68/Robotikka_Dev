6e6f502438e93ba6a91c1d2f6a605e1d3a401ed3
oshadaera68 <mindartlk010@gmail.com>
2023-05-09 22:48:13 +0530

    updated

--- src/com/devstack/pos/controller/CustomerFormController.java
+++ src/com/devstack/pos/controller/CustomerFormController.java
@@ -1,23 +1,33 @@
 package com.devstack.pos.controller;
 
 import com.devstack.pos.dao.DatabaseAccessCode;
+import com.devstack.pos.dto.CustomerDto;
+import com.devstack.pos.view.tm.CustomerTm;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class CustomerFormController {
 
     public AnchorPane context;
     public JFXTextField txtEmail;
     public JFXTextField txtName;
+    public JFXButton btnSaveUpdate;
     public JFXTextField txtContact;
     public JFXTextField txtSalary;
-    public TableView tblCustomer;
+    public TableView<CustomerTm> tblCustomer;
     public TableColumn colId;
     public TableColumn colEmail;
     public TableColumn colName;
@@ -25,33 +35,138 @@ public class CustomerFormController {
     public TableColumn colSalary;
     public TableColumn colOperate;
     public JFXTextField txtSearch;
+    private String searchText = "";
 
-    public void btnBackToHomeOnAction(ActionEvent actionEvent) {
+    public void btnBackToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashboardForm");
     }
 
     public void btnManageLoyaltyCardsOnAction(ActionEvent actionEvent) {
     }
 
     public void btnNewCustomerOnAction(ActionEvent actionEvent) {
+        txtEmail.setEditable(true);
+        btnSaveUpdate.setText("Save Customer");
+        clearFields();
+    }
+
+    public void initialize() throws SQLException, ClassNotFoundException {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colOperate.setCellValueFactory(new PropertyValueFactory<>("deleteButton"));
+
+        loadAllCustomers(searchText);
+
+        tblCustomer.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    if (newValue != null) {
+                        setData(newValue);
+                    }
+                });
+
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+            searchText=newValue;
+            try {
+                loadAllCustomers(searchText);
+            } catch (SQLException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void setData(CustomerTm newValue) {
+        txtEmail.setEditable(false);
+        btnSaveUpdate.setText("Update Customer");
+        txtEmail.setText(newValue.getEmail());
+        txtName.setText(newValue.getName());
+        txtSalary.setText(String.valueOf(newValue.getSalary()));
+        // txtSalary.setText(""+newValue.getSalary());
+        txtContact.setText(newValue.getContact());
+    }
+
+    private void loadAllCustomers(String searchText) throws SQLException, ClassNotFoundException {
+        ObservableList<CustomerTm> observableList = FXCollections.observableArrayList();
+        int counter=1;
+        for (CustomerDto dto:
+                searchText.length()>0?DatabaseAccessCode.searchCustomers(searchText):DatabaseAccessCode.findAllCustomers()){
+            Button btn = new Button("Delete");
+            CustomerTm tm = new CustomerTm(
+                    counter,dto.getEmail(), dto.getName(), dto.getContact(), dto.getSalary(),
+                    btn
+            );
+            observableList.add(tm);
+            counter++;
+
+            btn.setOnAction((e)->{
+                try{
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"Are you sure?", ButtonType.YES,ButtonType.NO);
+                    Optional<ButtonType> selectedButtonType = alert.showAndWait();
+                    if (selectedButtonType.equals(ButtonType.YES)){
+                        if (DatabaseAccessCode.deleteCustomer(dto.getEmail())){
+                            new Alert(Alert.AlertType.CONFIRMATION, "Customer Deleted!").show();
+                            loadAllCustomers(searchText);
+                        }else{
+                            new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+                        }
+                    }
+                }catch (SQLException | ClassNotFoundException exception){
+                    exception.printStackTrace();
+                    new Alert(Alert.AlertType.ERROR, exception.getMessage()).show();
+                }
+            });
+
+        }
+        tblCustomer.setItems(observableList);
     }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
-        try{
-            if (
-                    DatabaseAccessCode.createCustomer(
-                            txtEmail.getText(),txtName.getText(),
-                            txtContact.getText(),Double.parseDouble(txtSalary.getText())
-                    )
-            ){
-                new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved!").show();
-                clearFields();
-            }else{
-                new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+        try {
+            if (btnSaveUpdate.getText().equals("Save Customer")) {
+                if (
+                        DatabaseAccessCode.createCustomer(
+                                txtEmail.getText(), txtName.getText(),
+                                txtContact.getText(), Double.parseDouble(txtSalary.getText())
+                        )
+                ) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved!").show();
+                    clearFields();
+                    loadAllCustomers(searchText);
+                } else {
+                    new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+                }
+            } else {
+                if (
+                        DatabaseAccessCode.updateCustomer(
+                                txtEmail.getText(), txtName.getText(),
+                                txtContact.getText(), Double.parseDouble(txtSalary.getText())
+                        )
+                ) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Customer Updated!").show();
+                    clearFields();
+                    loadAllCustomers(searchText);
+                    //---------
+                    txtEmail.setEditable(true);
+                    btnSaveUpdate.setText("Save Customer");
+                } else {
+                    new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+                }
             }
-        }catch ( ClassNotFoundException | SQLException e){
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
             new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
         }
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.centerOnScreen();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/" + url + ".fxml")))
+        );
     }
 
     private void clearFields() {
--- src/com/devstack/pos/dao/DatabaseAccessCode.java
+++ src/com/devstack/pos/dao/DatabaseAccessCode.java
@@ -5,6 +5,7 @@ import com.devstack.pos.dto.UserDto;
 import com.devstack.pos.util.PasswordManager;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseAccessCode {
@@ -25,7 +26,7 @@ public class DatabaseAccessCode {
         PreparedStatement pstm = con.prepareStatement(sql);
         pstm.setString(1, email);
         ResultSet rst = pstm.executeQuery();
-        if (rst.next()){
+        if (rst.next()) {
             return new UserDto(
                     rst.getString(1),
                     rst.getString(2)
@@ -34,32 +35,101 @@ public class DatabaseAccessCode {
         }
         return null;
     }
+
     //====Customer management===============
-    public static boolean createCustomer(String email,String name, String contact, double salary) throws ClassNotFoundException, SQLException {
+    public static boolean createCustomer(String email, String name, String contact, double salary) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
-                "root","1234");
-        String sql="INSERT INTO customer VALUES (?,?,?,?)";
+                "root", "1234");
+        String sql = "INSERT INTO customer VALUES (?,?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, email);
         preparedStatement.setString(2, name);
         preparedStatement.setString(3, contact);
         preparedStatement.setDouble(4, salary);
-        return preparedStatement.executeUpdate()>0;
+        return preparedStatement.executeUpdate() > 0;
     }
-    public static boolean updateCustomer(String email,String name, String contact, double salary){
-        return false;
+
+    public static boolean updateCustomer(String email, String name, String contact, double salary) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root", "1234");
+        String sql = "UPDATE customer SET name=?, contact=?, salary=? WHERE email=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, contact);
+        preparedStatement.setDouble(3, salary);
+        preparedStatement.setString(4, email);
+        return preparedStatement.executeUpdate() > 0;
     }
-    public static CustomerDto findCustomer(String email){
+
+    public static CustomerDto findCustomer(String email) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root", "1234");
+        String sql = "SELECT * FROM customer WHERE email=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        ResultSet rst = preparedStatement.executeQuery();
+        if (rst.next()) {
+            return new CustomerDto(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getDouble(4)
+            );
+        }
         return null;
     }
-    public static boolean deleteCustomer(String email){
-        return false;
+
+    public static boolean deleteCustomer(String email) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root", "1234");
+        String sql = "DELETE FROM customer WHERE email=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        return preparedStatement.executeUpdate() > 0;
     }
-    public static List<CustomerDto> findAllCustomers(){
-        return null;
+
+    public static List<CustomerDto> findAllCustomers() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root", "1234");
+        String sql = "SELECT * FROM customer";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<CustomerDto> dtos = new ArrayList<>();
+        while (resultSet.next()) {
+            dtos.add(new CustomerDto(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getDouble(4)
+            ));
+        }
+        return dtos;
     }
-    public static List<CustomerDto> searchCustomers(String searchText){
-        return null;
+
+    public static List<CustomerDto> searchCustomers(String searchText) throws SQLException, ClassNotFoundException {
+        searchText = "%" + searchText + "%";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root", "1234");
+        String sql = "SELECT * FROM customer WHERE email LIKE ? || name LIKE ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, searchText);
+        preparedStatement.setString(2, searchText);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<CustomerDto> dtos = new ArrayList<>();
+        while (resultSet.next()) {
+            dtos.add(new CustomerDto(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getDouble(4)
+            ));
+        }
+        return dtos;
     }
 }
